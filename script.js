61e40eee65908b49fb5944c8337ac135e67b9059
Justin Kahn <71735000+justinthegithub@users.noreply.github.com>
2024-05-04 15:39:46 +0000

    requriements added and form

---
+++ script.js
@@ -0,0 +1,14 @@
+//    ✕ Adds a single new employee's data to the table 
+//Adds multiple new employees' data to the table
+//Clears out the form inputs after a new employee is submitted 
+//Updates the total monthly salary value when a single employee is added 
+//Updates the total monthly salary value when multiple employees are added 
+//Applies the 'over-budget' CSS class to the footer when the total monthly salary exceeds $20,000 
+// Removes the correct employee table row when a delete button is clicked 
+
+
+/**When the "Submit" button is clicked:
+
+A new employee row should be added to the table.
+The footer's total monthly cost should be updated.
+The form inputs should be cleared out./ */
