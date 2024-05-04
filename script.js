5ef33b5b1c1510ba06bd41c496b5ad20222b0b3f
Justin Kahn <71735000+justinthegithub@users.noreply.github.com>
2024-05-04 18:12:53 +0000

    added comments from slack

--- script.js
+++ script.js
@@ -12,3 +12,24 @@
 A new employee row should be added to the table.
 The footer's total monthly cost should be updated.
 The form inputs should be cleared out./ */
+
+let firstNameInput = document.getElementById("firstNameInput");
+let lastNameInput = document.getElementById("lastNameInput");
+let idInput = document.getElementById("idInput");
+let titleInput = document.getElementById("titleInput")
+let annualSalaryInput = document.getElementById("annualSalaryInput");
+
+let submit = document.getElementById("submit");
+
+let table =document.getElementById(employeeTable)
+table.innerHTML = 
+  <td>${firstNameInput}/td>
+<td>${lastNameInput}</td>
+  <td>${idInput}</td>
+    <td>${titleInput}</td>
+    <td>${annualSalaryInput}</td>
+
+
+
+
+
